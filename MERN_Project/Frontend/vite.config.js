f7e46e4e6657dd3be969604435201664c613f017
Chetan Kolhe <Chetankolhe1020@gmail.com>
2025-04-09 16:08:34 +0530

    redirects

--- MERN_Project/Frontend/vite.config.js
+++ MERN_Project/Frontend/vite.config.js
@@ -7,4 +7,14 @@ export default defineConfig({
     outDir: 'dist', // Ensure the output folder is set
     emptyOutDir: true, // Clears old files before building
   },
+  closeBundle() {
+    const from = path.resolve(__dirname, 'public/_redirects');
+    const to = path.resolve(__dirname, 'dist/_redirects');
+    if (existsSync(from)) {
+      copyFileSync(from, to);
+      console.log('✅ Copied _redirects to dist/');
+    } else {
+      console.warn('⚠️ _redirects file not found in public/');
+    }
+  }
 })
